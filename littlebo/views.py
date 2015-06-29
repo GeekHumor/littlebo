e5a0540e6057cae1bbc0dcb0ef1ffe6c8bebe960
Kouni <kouni@LCSE.org>
2015-06-30 00:05:20 +0800

    add login error...

--- littlebo/views.py
+++ littlebo/views.py
@@ -1,7 +1,28 @@
 # -*- coding: UTF-8 -*-
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 from django.shortcuts import render_to_response
 
-def orz(request):
-    return HttpResponse('小波~ 我在這！')
+def index(request):
+    return render_to_response('index.html', RequestContext(request, locals()))
+
+def login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/index/')
+
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render_to_response('login.html', RequestContext(request, locals()))
+
+def logout(request):
+    auth.logout(request)
+    return  HttpResponseRedirect('/index/')
